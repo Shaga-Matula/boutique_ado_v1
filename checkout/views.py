4af1c41207abd2190d1992c776d0104d32be7999
Paul-Gleeson <Shagamatula@gmail.com>
2023-09-28 11:55:05 +0000

    slipe

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
-
-from .forms import OrderForm
-from bag.contexts import bag_contents
-
-
 import stripe
+from bag.contexts import bag_contents
+from .forms import OrderForm
+
+
+
+
 
 
 def checkout(request):
@@ -33,8 +34,10 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51NvFG2KnbYEnx5OfpEnDXH3ibrWRTB8pTuGf0njsHe3s3FCHAkz4Xh2NG2N7v84Fb6tSYDg5rHPVsUBs9rUZNnQa00XecMkKdS',
-        'client_secret': 'sk_test_51NvFG2KnbYEnx5OfqOWebbmbUvnyzEAcD9Ka45GSWeFbGtiht2PLMXxinXv6gdTdLBtFLXCgELDqouRAjVu0wpwN00hDeaZrME',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+        # 'stripe_public_key': 'pk_test_51NvFG2KnbYEnx5OfpEnDXH3ibrWRTB8pTuGf0njsHe3s3FCHAkz4Xh2NG2N7v84Fb6tSYDg5rHPVsUBs9rUZNnQa00XecMkKdS',
+        # 'client_secret': 'sk_test_51NvFG2KnbYEnx5OfqOWebbmbUvnyzEAcD9Ka45GSWeFbGtiht2PLMXxinXv6gdTdLBtFLXCgELDqouRAjVu0wpwN00hDeaZrME',
     }
 
     return render(request, template, context)
