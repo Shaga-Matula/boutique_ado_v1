40cdc533bd0d3245f65c9dcac0f968e8116901f6
Paul-Gleeson <Shagamatula@gmail.com>
2023-09-28 09:22:22 +0000

    stripe setup

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,40 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+    amount=stripe_total,
+    currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': 'pk_test_51NvFG2KnbYEnx5OfpEnDXH3ibrWRTB8pTuGf0njsHe3s3FCHAkz4Xh2NG2N7v84Fb6tSYDg5rHPVsUBs9rUZNnQa00XecMkKdS',
-        'client_secret': 'test client secret',
+        'client_secret': 'sk_test_51NvFG2KnbYEnx5OfqOWebbmbUvnyzEAcD9Ka45GSWeFbGtiht2PLMXxinXv6gdTdLBtFLXCgELDqouRAjVu0wpwN00hDeaZrME',
     }
 
     return render(request, template, context)
