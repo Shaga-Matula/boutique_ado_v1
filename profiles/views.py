536039a02d8508104f51d0265f15769667a1260e
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-04 15:37:08 +0000

    add to bag

--- profiles/views.py
+++ profiles/views.py
@@ -15,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -32,16 +34,15 @@ def profile(request):
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
-    message.info(request, (
-        f'This is a past confirmation messsage for order number {order_number}.'
-        'A confirmation email was sent on the order date'
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
     ))
 
-
-    template= 'checkout/checkout_success.html'
-    context= {
+    template = 'checkout/checkout_success.html'
+    context = {
         'order': order,
-        'from_profile' : True,
+        'from_profile': True,
     }
 
     return render(request, template, context)
